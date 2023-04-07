b4a9f2ee17cddc06de125aa4ffaead920519e4cd
DanSeq995 <danilosequino@gmail.com>
2023-04-07 16:09:50 +0200

    Project Init

---
+++ ReportCheck/ReportCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReportCheckApp.swift
+//  ReportCheck
+//
+//  Created by Danilo Sequino on 07/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ReportCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
